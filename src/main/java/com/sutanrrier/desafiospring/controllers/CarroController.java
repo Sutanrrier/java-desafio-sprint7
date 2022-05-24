ee654560b6f6215f294f42aec3f1ebbe6bd89375
Sutanrrier <pabloryanbr0@gmail.com>
2022-05-24 15:25:10 -0300

    Update Car Method created

--- src/main/java/com/sutanrrier/desafiospring/controllers/CarroController.java
+++ src/main/java/com/sutanrrier/desafiospring/controllers/CarroController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,9 +43,9 @@ public class CarroController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Carro> saveCar(@RequestBody Carro carro){
-		carro.setDataCriacao(Date.valueOf(LocalDate.now()));
-		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(carro));
+	public ResponseEntity<Carro> saveCar(@RequestBody Carro obj){
+		obj.setDataCriacao(Date.valueOf(LocalDate.now()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(obj));
 	}
 	
 	@DeleteMapping(value="/{id}")
@@ -55,5 +56,14 @@ public class CarroController {
 		}
 		service.delete(carro.get());
 		return ResponseEntity.status(HttpStatus.OK).body("Carro deletado com sucesso!");
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Object> updateCar(@PathVariable Integer id, @RequestBody Carro obj) {
+		Optional<Carro> carro = service.findById(id);
+		if(!carro.isPresent() || obj.getId() != id) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Carro não encontrado no banco de dados!");
+		}
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(obj));
 	}
 }
