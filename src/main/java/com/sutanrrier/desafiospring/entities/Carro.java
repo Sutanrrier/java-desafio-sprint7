ddedc98578370d19b3bf26038fca6696b62ed2fa
Sutanrrier <pabloryanbr0@gmail.com>
2022-05-25 13:51:52 -0300

    Added JoinColumn annotation in User Entity

--- src/main/java/com/sutanrrier/desafiospring/entities/Carro.java
+++ src/main/java/com/sutanrrier/desafiospring/entities/Carro.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -32,6 +33,7 @@ public class Carro {
 	private Date dataCriacao;
 	
 	@ManyToOne
+	@JoinColumn(name="estacionamento_id")
 	private Estacionamento estacionamento;
 	
 	//Construtores
