cad4cdaf8097ceed37aff106178a98ae0dbf07df
Sutanrrier <pabloryanbr0@gmail.com>
2022-06-02 18:39:34 -0300

    HttpHeaders updated

--- src/main/java/com/sutanrrier/desafiospring/headers/MyHeaders.java
+++ src/main/java/com/sutanrrier/desafiospring/headers/MyHeaders.java
@@ -5,8 +5,9 @@ import org.springframework.http.HttpHeaders;
 public class MyHeaders {
 	public static HttpHeaders getHeaders() {
 		HttpHeaders headers = new HttpHeaders();
-		headers.set("Access-Control-Allow-Origin", "http://127.0.0.1:5500/frontend/pages/cadastrarCarro.html");
 		headers.set("Access-Control-Allow-Credentials", "true");
+		headers.set("Access-Control-Allow-Origin", "*");
+		headers.set("Acess-Control-Allow-Methods", "GET,POST,PUT,DELETE");
 		return headers;
 	}
 }
