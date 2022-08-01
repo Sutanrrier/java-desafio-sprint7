579ce0d8dfdcf926dfcd65639bad8bd1eccae8c2
Sutanrrier <pabloryanbr0@gmail.com>
2022-08-01 14:29:15 -0300

    Return CarroController to RestController Annotation

--- src/main/java/com/sutanrrier/desafiospring/controllers/CarroController.java
+++ src/main/java/com/sutanrrier/desafiospring/controllers/CarroController.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,11 +19,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.sutanrrier.desafiospring.entities.Carro;
 import com.sutanrrier.desafiospring.services.CarroService;
 
-@Controller
+@RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(value = "/carros")
 public class CarroController {
